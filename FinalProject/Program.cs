bb44e142e02a80e29e2bee3d1d9296a74bfd0b36
Prasad Deshmukh <prasad.deshmukh@waiin.com>
2022-06-29 20:13:39 +0530

    change program.ts allow

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -27,6 +27,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//********************************************************************************************
+ app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+//********************************************************************************************
+
 app.UseAuthorization();
 
 app.MapControllers();
